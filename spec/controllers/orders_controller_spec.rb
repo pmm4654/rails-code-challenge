6b8996db8bb3403e8b3777674b1280cacdb36c89
Patrick MacMurchy <pmm4654@gmail.com>
2018-11-11 08:49:24 -0700

    Make sure the spec would catch a breaking index page again

--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe OrdersController, type: :controller do
+  render_views
   describe '#index' do
+    let!(:order) { Order.create! }
     subject { get :index }
     it { is_expected.to have_http_status(:ok) }
+    it 'should have the "Orders" title and order id on the page' do       
+      expect(subject.body).to include('Orders')
+      expect(subject.body).to include("#{order.id}")
+    end
   end
 
   describe '#show' do
