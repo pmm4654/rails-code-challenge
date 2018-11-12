20fafd0a6dc7283038ba4cd172ddf3d559d0ff0b
Patrick MacMurchy <pmm4654@gmail.com>
2018-11-12 15:52:41 -0700

    Modify models and update controller to prep for CRUD

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,6 +1,7 @@
 class LineItem < ApplicationRecord
-  belongs_to :order
+  belongs_to :order, inverse_of: :line_items
   belongs_to :widget
 
   validates :order, :quantity, :unit_price, :widget, presence: true
+
 end
--- app/models/widget.rb
+++ app/models/widget.rb
@@ -1,2 +1,7 @@
 class Widget < ApplicationRecord
+  has_many :line_items, dependent: :destroy
+
+  def self.select_options
+    order(:name).pluck(:name, :id)
+  end
 end
