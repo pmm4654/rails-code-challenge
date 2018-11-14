31713d41929a3efafa187b1cd3ea46082e7f4a0f
Patrick MacMurchy <pmm4654@gmail.com>
2018-11-13 17:19:46 -0700

    Update seeds to have line items

--- db/seed_helpers/order_creation_helper.rb
+++ db/seed_helpers/order_creation_helper.rb
@@ -4,13 +4,25 @@ module SeedHelpers
       date_range ||= 2015..DateTime.now.year
       number_of_orders.times do |order_number|
         Order.create!() unless shipping
-        Order.create!(shipped_at: random_date_in_year(date_range)) if shipping
+        Order.create!(shipped_at: random_date_in_year(date_range), line_items_attributes: build_widgets) if shipping
       end
+    end
+
+    def self.build_widgets 
+      widgets = []
+      rand(10).times do 
+        widgets << {widget_id: widget_ids.sample, quantity: rand(100), unit_price: (100)}
+      end
+      widgets
     end
 
     def self.random_date_in_year(year)
       return rand(DateTime.civil(year.min, 1, 1)..DateTime.civil(year.max, 12, 31)) if year.kind_of?(Range)
       rand(DateTime.civil(year, 1, 1)..DateTime.civil(year, 12, 31))
+    end
+
+    def self.widget_ids
+      @widget_ids ||= Widget.pluck(:id)
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,8 @@ require Rails.root.join('db', 'seed_helpers', 'order_creation_helper.rb')
 require Rails.root.join('db', 'seed_helpers', 'widget_creation_helper.rb')
 include SeedHelpers
 
+WidgetCreationHelper::create_widgets!(10)
+
 OrderCreationHelper::create_orders!(5, shipping: false)
 OrderCreationHelper::create_orders!(5)
 
-WidgetCreationHelper::create_widgets!(10)
